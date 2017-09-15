9d17659f8ed25bc957fd85b13ba8519f56de45a8
Alina L <alodahl@pointloma.edu>
2017-09-14 17:06:35 -0700

    finished ajax function in index.js, added links in html

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
+const mykey = config.API_key;
+console.log(mykey);
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function getDataFromApi(searchTerm, callback) {
-  const query = {
-    q: `${searchTerm} in:name`,
-    per_page: 10
-  }
-  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+function getDataFromApi(searchTerm , callback) {
+
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    key: `${mykey}`,
+    data: {
+      q: `${searchTerm} in:name`,
+      per_page: 10,
+      part: 'snippet'
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+console.log(settings);
+$.ajax(settings);
 }
+
 
 function renderResult(result) {
-  return `
-    <div>
+  return `<div>${result}</div>`;
+    /*<div>
       <h2>
-      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
-      <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
-      <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
-    </div>
-  `;
+      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.title}</a>
+      by <a class="js-user-name" href="${result.html_url}" target="_blank">${result.login}</a>
+    </h2>
+      <p>Number of watchers: <span class="js-watchers-count">${result.activity}</span></p>
+
+    </div>*/
+  ;
 }
 
-function displayGitHubSearchData(data) {
+function displaySearchData(data) {
   const results = data.items.map((item, index) => renderResult(item));
   $('.js-search-results').html(results);
 }
@@ -31,7 +46,7 @@ function watchSubmit() {
     const query = queryTarget.val();
     // clear out the input
     queryTarget.val("");
-    getDataFromApi(query, displayGitHubSearchData);
+    getDataFromApi(query, displaySearchData);
   });
 }
 
